d320403e840f094ce1f0308589f016b1ba32a675
Makarew <makarew@gmail.com>
2024-03-27 11:34:58 -0600

    Initial Commit

---
+++ P06Cutscenes/StageMetadataNew.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using P06ML.Metadata;
+
+namespace StageExtensions
+{
+    public class StageMetadataNew : StageMetadata
+    {
+        public RequireSE RequireStageExtensions { get; set; } = 0;
+        public string DescriptionSE { get; set; } = "N/A";
+        public string AssetBundleSE { get; set; } = "N/A";
+        public string TitleSE { get; set; } = "N/A";
+
+        public enum RequireSE
+        {
+            Not_Required,
+            Recommended,
+            Required
+        }
+
+        public static StageMetadataNew Load(string path)
+        {
+            StageMetadataNew stageMetadata = MetadataBase.Load<StageMetadataNew>(path, false);
+            if (!string.IsNullOrEmpty(stageMetadata.Thumbnail))
+            {
+                stageMetadata.Thumbnail = Path.Combine(stageMetadata.Location, stageMetadata.Thumbnail);
+            }
+            if (!string.IsNullOrEmpty(stageMetadata.LoadingThumbnail))
+            {
+                stageMetadata.LoadingThumbnail = Path.Combine(stageMetadata.Location, stageMetadata.LoadingThumbnail);
+            }
+            return stageMetadata;
+        }
+    }
+}
